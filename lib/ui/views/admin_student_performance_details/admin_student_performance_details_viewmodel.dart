f5c121fa18eeb1de5fcae7ad9d489f6b1c957f6c
emmanuel-mutua <emmamulwa2020@gmail.com>
2024-06-12 16:44:03 +0300

    admin add marks recommedations

--- lib/ui/views/admin_student_performance_details/admin_student_performance_details_viewmodel.dart
+++ lib/ui/views/admin_student_performance_details/admin_student_performance_details_viewmodel.dart
@@ -1,3 +1,4 @@
+import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:stacked/stacked.dart';
 
 import '../../../app/app.locator.dart';
@@ -8,8 +9,47 @@ class AdminStudentPerformanceDetailsViewModel extends BaseViewModel {
   final _adminDashboardService = locator<AdminDashboardService>();
   Stream<List<StudentsRegisteredUnitsModel>>? _studentUnitsStream;
   Stream<List<StudentsRegisteredUnitsModel>>? get studentUnitsStream => _studentUnitsStream;
-  void fetchStudentUnits(String semesterStage, String studentUid) {
+
+  String? _recommendationText;
+  String? get recommendationText => _recommendationText;
+
+  // void fetchStudentUnits(String semesterStage, String studentUid) {
+  //   _studentUnitsStream = _adminDashboardService.getStudentUnits(semesterStage, studentUid);
+  //   notifyListeners();
+  // }
+  // Future<void> generateRecommendations(List<StudentsRegisteredUnitsModel> units) async {
+  //   final totalMarksList = units.map((unit) => unit.totalMarks).toList();
+  //   // Feed the totalMarksList to your recommendation model or service
+  //   final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: "AIzaSyAPyNw4jkSHZWCN7Kgln5jMHV5aMqkDYDE");
+  //   final prompt = 'Recommended performance in 2 lines based on the following marks: ${totalMarksList.join(", ")}.';
+  //   final content = [Content.text(prompt)];
+  //   final response = await model.generateContent(content);
+  //   _recommendationText = response.text;
+  //   notifyListeners();
+  // }
+  void fetchStudentUnitsAndGenerateRecommendations(String semesterStage, String studentUid) async {
     _studentUnitsStream = _adminDashboardService.getStudentUnits(semesterStage, studentUid);
     notifyListeners();
+
+    try {
+      final units = await _studentUnitsStream!.first;
+      generateRecommendations(units);
+    } catch (e) {
+      print('Error fetching student units or generating recommendations: $e');
+    }
   }
+
+  Future<void> generateRecommendations(List<StudentsRegisteredUnitsModel> units) async {
+    final totalMarksList = units.map((unit) => unit.totalMarks).toList();
+    // Feed the totalMarksList to your recommendation model or service
+    final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: "AIzaSyAPyNw4jkSHZWCN7Kgln5jMHV5aMqkDYDE");
+    final prompt = 'Recommended performance in 2 lines based on the following marks: ${totalMarksList.join(", ")}.';
+    final content = [Content.text(prompt)];
+    final response = await model.generateContent(content);
+    _recommendationText = response.text;
+    notifyListeners();
+  }
+
+
+
 }
