5be6135cb9f515d51b7a39263f047a185230e183
Erasto-M <kyalo.erastus21@students.dkut.ac.ke>
2024-06-24 11:30:48 +0300

    made changes

--- lib/app/app.bottomsheets.dart
+++ lib/app/app.bottomsheets.dart
@@ -6,8 +6,6 @@
 
 import 'package:stacked_services/stacked_services.dart';
 
-import '../ui/bottom_sheets/student_register_unit/student_register_unit_sheet.dart';
-import '../ui/bottom_sheets/update_profile/update_profile_sheet.dart';
 import 'app.locator.dart';
 import '../ui/bottom_sheets/add_unit/add_unit_sheet.dart';
 import '../ui/bottom_sheets/edit_student_marks/edit_student_marks_sheet.dart';
@@ -19,8 +17,6 @@ enum BottomSheetType {
   addUnit,
   editUnit,
   editStudentMarks,
-  studentRegisterUnit,
-  updateProfile,
 }
 
 void setupBottomSheetUi() {
@@ -35,10 +31,6 @@ void setupBottomSheetUi() {
         EditUnitSheet(request: request, completer: completer),
     BottomSheetType.editStudentMarks: (context, request, completer) =>
         EditStudentMarksSheet(request: request, completer: completer),
-         BottomSheetType.studentRegisterUnit: (context, request, completer) =>
-        StudentRegisterUnitSheet(request: request, completer: completer),
-    BottomSheetType.updateProfile: (context, request, completer) =>
-        UpdateProfileSheet(request: request, completer: completer),
   };
 
   bottomsheetService.setCustomSheetBuilders(builders);
