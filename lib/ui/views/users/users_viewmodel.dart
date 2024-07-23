55491d43776eccce83219410480e06dfc78502f9
emmanuel-mutua <emmamulwa2020@gmail.com>
2024-07-23 09:56:26 +0300

    add ability to call/email user

--- lib/ui/views/users/users_viewmodel.dart
+++ lib/ui/views/users/users_viewmodel.dart
@@ -3,6 +3,7 @@ import 'package:examify/models/usersModel.dart';
 import 'package:examify/services/authentication_service.dart';
 import 'package:examify/ui/widgets/common/users/users.dart';
 import 'package:stacked/stacked.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class UsersViewModel extends BaseViewModel {
   final _authService = locator<AuthenticationService>();
@@ -14,7 +15,29 @@ class UsersViewModel extends BaseViewModel {
     notifyListeners();
   }
 
-  void email({required email}) {}
+  void email({required String email}) async {
+    final Uri emailUri = Uri(
+      scheme: 'mailto',
+      path: email,
+    );
+    if (await canLaunch(emailUri.toString())) {
+      await launch(emailUri.toString());
+    } else {
+      // Handle the error gracefully
+      print('Could not launch $emailUri');
+    }
+  }
 
-  void call({required phoneNumber}) {}
+  void call({required String phoneNumber}) async {
+    final Uri phoneUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    if (await canLaunch(phoneUri.toString())) {
+      await launch(phoneUri.toString());
+    } else {
+      // Handle the error gracefully
+      print('Could not launch $phoneUri');
+    }
+  }
 }
